9a7240b26b3c9e77999b09a9392bbe6437bf8e8f
AngelMarquesGarcia <117300926+AngelMarquesGarcia@users.noreply.github.com>
2023-05-30 15:28:39 +0200

    Implemented King Movement

--- src/pieces/King.java
+++ src/pieces/King.java
@@ -5,6 +5,7 @@
 
 package pieces;
 
+import chessproject.GameBoard;
 import java.util.ArrayList;
 import java.util.List;
 import utilities.Coordinates;
@@ -18,7 +19,49 @@ public class King extends ChessPiece {
     
     public static List<Coordinates> updateAvailableMoves(Coordinates p, WorB c){
         List<Coordinates> coords = new ArrayList<>();
+        Coordinates[] moves = getMoveset();
+        for (Coordinates move : moves) {
+            tryMoves(coords, move, p, c);
+        }
         return coords;
+    }
+
+    private static Coordinates[] getMoveset() {
+        Coordinates[] moveset = new Coordinates[8];
+        moveset[0] = new Coordinates(1, 1, false);
+        moveset[1] = new Coordinates(1, -1, false);
+        moveset[2] = new Coordinates(-1, 1, false);
+        moveset[3] = new Coordinates(-1, -1, false);
+        moveset[4] = new Coordinates(0, 1, false);
+        moveset[5] = new Coordinates(0, -1, false);
+        moveset[6] = new Coordinates(-1, 0, false);
+        moveset[7] = new Coordinates(1, 0, false);
+        return moveset;
+    }
+
+    /**
+     * Adds to coords all the Coordinates that follow pos+n*move, where
+     * n takes integer values 1,2,3...
+     * Stops when a piece is found or when trying to access inexistent coordinates
+     * If a piece is found, it checks its color. If it's different from color,
+     * then that piece's position is added to coords.
+     * 
+     * @param coords
+     * @param move
+     * @param pos
+     * @param color 
+     */
+    private static void tryMoves(List<Coordinates> coords, Coordinates move, Coordinates pos, WorB color) {
+        try {
+            Coordinates newPos = pos.clone();
+            newPos.sum(move);
+            ChessPiece piece = GameBoard.at(newPos);
+            if (piece == null || ! piece.isColor(color)){
+                coords.add(newPos);
+            }
+        } catch (IndexOutOfBoundsException ex) {
+
+        }
     }
 
     private static final String className = "K";
