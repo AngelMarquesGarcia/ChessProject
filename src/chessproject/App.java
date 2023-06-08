eea4d4cd085e8a3fc6e65bcdc5b95c89a966b8bd
AngelMarquesGarcia <117300926+AngelMarquesGarcia@users.noreply.github.com>
2023-06-08 20:21:02 +0200

    print que tal

--- src/chessproject/App.java
+++ src/chessproject/App.java
@@ -23,6 +23,8 @@ public class App {
         
         System.out.println("Adios, mundo");
         
+        System.out.println("Que tal, mundo");
+        
         //Schedule a job for the event dispatch thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
