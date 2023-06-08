97ff580d6ba1bba84f4129499eb1f262097ee543
AngelMarquesGarcia <117300926+AngelMarquesGarcia@users.noreply.github.com>
2023-06-08 20:21:28 +0200

    print muy bien

--- src/chessproject/App.java
+++ src/chessproject/App.java
@@ -25,6 +25,8 @@ public class App {
         
         System.out.println("Que tal, mundo");
         
+        System.out.println("Muy bien, gracias");
+        
         //Schedule a job for the event dispatch thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
