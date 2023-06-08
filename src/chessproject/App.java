5b1bd809d60605944074736dbe81c67db93a3cc4
AngelMarquesGarcia <117300926+AngelMarquesGarcia@users.noreply.github.com>
2023-06-08 20:15:55 +0200

    print hola

--- src/chessproject/App.java
+++ src/chessproject/App.java
@@ -17,6 +17,10 @@ import pieces.ChessPiece;
 public class App {
     
     public App(){
+        
+        System.out.println("Hola");
+        
+        
         //Schedule a job for the event dispatch thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
